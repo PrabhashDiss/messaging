4669812d16dbef2e5ee8b34c2dcff2344b6e7484
PrabhashDiss <101409417+PrabhashDiss@users.noreply.github.com>
2024-04-17 21:17:43 +0530

    Add Prost runtime support and encode/decode functionality for User and Message structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+// Import Prost's runtime support
+use prost::Message as ProstMessage;
+
 // Include the `messaging` module, which is generated from messaging.proto.
 pub mod messaging {
     include!(concat!(env!("OUT_DIR"), "/messaging.rs"));
@@ -11,7 +14,11 @@ fn main() {
     user.id = "123".to_string();
     user.username = "user123".to_string();
 
-    println!("{:?}", user);
+    let mut user_bytes = Vec::new();
+    user.encode(&mut user_bytes).unwrap();
+
+    let decoded_user = User::decode(&user_bytes[..]).unwrap();
+    println!("{:?}", decoded_user);
 
     let mut message = Message::default();
     message.id = "456".to_string();
@@ -19,5 +26,9 @@ fn main() {
     message.receiver_id = "789".to_string();
     message.content = "Hello, world!".to_string();
 
-    println!("{:?}", message);
+    let mut message_bytes = Vec::new();
+    message.encode(&mut message_bytes).unwrap();
+
+    let decoded_message = Message::decode(&message_bytes[..]).unwrap();
+    println!("{:?}", decoded_message);
 }
